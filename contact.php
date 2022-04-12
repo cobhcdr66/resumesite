6a563c0e7fe09cf14e38054c0215bd9b348f27a9
cobhcdr66 <motsayjoe@gmail.com>
2022-04-12 12:07:17 -0600

    Add php

---
+++ contact.php
@@ -0,0 +1,7 @@
+First Name: <input type="text" name="firstname" value="<?php echo $firstname;?>">
+
+Last Name: <input type="text" name="lastname" value="<?php echo $lastname;?>">
+
+E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+
+Comment: <input type="text" name="subject" value="<?php echo $subject;?>">
